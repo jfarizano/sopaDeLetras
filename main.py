7b18cfce2b0009f3af750d4e6edba7f681e8bc09
alejo47 <alejoaper47@gmail.com>
2018-10-16 08:34:13 -0300

    Agregada opcion 2, lectura de palabras por archivo y fixes

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import sys
 from random import *
 from math import *
 
-def main(list, tablero = []):
+"""
+Funcion principal. Se encarga de darle un menu al usuario y que pueda elegir si crear o buscar palabras en un tablero
+"""
+def main(lista, tablero = []):
 
-	mensajeBienvenida = """Bienvenido, ingrese un número para elegir la opción deseada:
-	1 - Crear sopa de letras a partir de una lista de palabras
-	2 - Buscar la posición de las palabras de una lista en una sopa de letras dada 
-	3 - Salir del programa
-	"""
-
-	print(mensajeBienvenida)
 	modo = 0
 
-	while modo != "1" and modo != "2" and modo != "3":
+	if len(sys.argv) > 1 and sys.argv[1] in ["1","2","3"]:
+		modo = sys.argv[1]
+	else:
+		mensajeBienvenida = """Bienvenido, ingrese un número para elegir la opción deseada:
+		1 - Crear sopa de letras a partir de una lista de palabras
+		2 - Buscar la posición de las palabras de una lista en una sopa de letras dada 
+		3 - Salir del programa
+		"""
+
+		print(mensajeBienvenida)
+
+	while modo not in ["1","2","3"]:
 		if modo != 0:
 			print("Opción inválida, ingrese un número válido")
 			modo = str(input("Modo deseado: "))
@@ -20,68 +28,78 @@ def main(list, tablero = []):
 			modo = str(input("Modo deseado: "))
 
 	if modo == "1":
-		crearSopa(list)
+		crearSopa(lista)
 	elif modo == "2":
-		buscarPalabras(list, tablero)
-	else:
+		buscarPalabras(lista, tablero)
+	elif modo == "3":
 		exit()
 
 # Código opción 1
 
-def crearSopa(list):
+"""
+Crea una sopa de letras en base a una lista de strings
+"""
+def crearSopa(lista):
 
 	direcciones = ["hori1", "hori2", "vert1", "vert2", "diag"]
-	tablero = crearTablero(list)
-	listaPalabras = sorted(list, key=len, reverse=True)
+	tablero = crearTablero(lista)
+	listaPalabras = sorted(lista, key=len, reverse=True)
 
 	for palabra in listaPalabras:
 		disponibles = []
-		while disponibles == []:
+		intentos = 0
+
+		while disponibles == [] and intentos <= 10:
 			direccion = choice(direcciones)
 			disponibles = lugaresDisponibles(tablero, palabra, direccion)
-		pos = choice(disponibles)
-		if direccion != "diag":
-			if direccion[-1] == "1":
-				colocarPalabra(tablero, palabra, direccion, pos)
-			else:
-				palabra = palabra[::-1]
-				colocarPalabra(tablero, palabra, direccion, pos)
+			intentos += 1
+		if intentos > 10:
+			print("No se pudo insertar: ", palabra)
 		else:
-			colocarPalabra(tablero, palabra, direccion, pos)
+			pos = choice(disponibles)
+			if direccion != "diag":
+				if direccion[-1] == "1":
+					colocarPalabra(tablero, palabra, direccion, pos)
+				else:
+					palabra = palabra[::-1]
+					colocarPalabra(tablero, palabra, direccion, pos)
+			else:
+				colocarPalabra(tablero, palabra, direccion, pos)
+	
+	imprimirTablero(rellenarTablero(tablero))
+	return rellenarTablero(tablero)
 
-	rellenarTablero(tablero)
-	print("\n")
-	imprimirTablero(tablero)
+"""
+Crea un tablero vacio del tamaño necesario en base a una lista de palabras
+"""
+def crearTablero(lista):
 
-def crearTablero(list):
-
-	rt = palabraMasLarga(list)
-	tablero = [""]*rt**2
+	tamaño = palabraMasLarga(lista) + randint(2,7)
+	tablero = [""]*tamaño**2
 	return tablero
 
+"""
+Imprime la sopa de letras en una forma placentera de leer para el usuario
+"""
 def imprimirTablero(tablero):
 
-	rt = int(sqrt(len(tablero)))
+	raiz = int(sqrt(len(tablero)))
 	filas = []
 	tableroStr = []
 
 	# Convierte la lista plana en lista de filas (matriz)
-	for i in range(0, rt):
-		filas += [tablero[i * rt: (i * rt) + rt]]
+	for i in range(0, raiz):
+		filas += [tablero[i * raiz: (i * raiz) + raiz]]
 	# Convierte las filas en string (ahora el tablero es lista de strings)
 	for i in range(0, len(filas)):
-		tableroStr += [listaAString(filas[i])]
+		tableroStr += [' '.join(filas[i])]
 	# Imprime cada fila como string
 	for str in tableroStr:
 		print(str)
 
-def listaAString(list):
-	str = ""
-	for i in range(0, len(list)):
-		str = str + list[i] + " "
-
-	return str
-
+"""
+Rellena los espacios vacios de una sopa de letras con letras al azar
+"""
 def rellenarTablero(tablero):
 
 	letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
@@ -89,75 +107,86 @@ def rellenarTablero(tablero):
 	for i in range(0, len(tablero)):
 		if tablero[i] == "":
 			tablero[i] = choice(letras)
+	return tablero
 
-def palabraMasLarga(list):
-
+"""
+Devuelve un entero con el largo del string de mayor longitud de una lista
+"""
+def palabraMasLarga(lista):
 	max = 0
-	for palabra in list:
+	for palabra in lista:
 		if len(palabra) > max:
 			max = len(palabra)
 	return max
 
+"""
+Devuelve una lista de los posibles lugares donde una palabra se puede insertar, dada una direccion.
+"""
 def lugaresDisponibles(tablero, palabra, direccion):
 
-	rt = int(sqrt(len(tablero)))
+	raiz = int(sqrt(len(tablero)))
 	length = len(palabra)
 	pos = []
 
 	if direccion == "diag":
 		for i in range(0, len(tablero)):
-			final = i + (length-1)*(rt+1)
-			if tablero[i:final+1:rt+1] == [""]*length and diagValida(tablero, palabra, i):
+			final = i + (length-1)*(raiz+1)
+			if tablero[i:final+1:raiz+1] == [""]*length and diagValida(tablero, palabra, i):
 				pos += [i]
-	elif direccion[:4] == "vert":
-		for i in range(0, (rt-length+1)*rt):
+	elif direccion[:4] == "veraiz":
+		for i in range(0, (raiz-length+1)*raiz):
 			if i == 0:
-				ultima = rt * length-1
-				columna = tablero[i:ultima:rt]
+				ultima = raiz * length-1
+				columna = tablero[i:ultima:raiz]
 				if columna == [""]*length:
 					pos += [i]
 			else:
-				ultima = (rt * length-1) + i
-				columna = tablero[i:ultima:rt]
+				ultima = (raiz * length-1) + i
+				columna = tablero[i:ultima:raiz]
 				if columna == [""]*length:
 					pos += [i]
 	elif direccion[:4] == "hori":
 		filas = []
-		for i in range(0, rt):
-			filas += [tablero[i * rt: (i * rt) + rt]]
-		for i in range(0, rt):
-			for j in range(0, rt-length+1):
+		for i in range(0, raiz):
+			filas += [tablero[i * raiz: (i * raiz) + raiz]]
+		for i in range(0, raiz):
+			for j in range(0, raiz-length+1):
 				if filas[i][j:j + length] == [""] * length:
-					pos += [(i*rt)+j]
+					pos += [(i*raiz)+j]
 	return pos
 
+"""
+Devuelve un booleano que indica si una palabra dada entra en la diagonal de la casilla del tablero dado
+"""
 def diagValida(tablero, palabra, casilla):
-
-	rt = int(sqrt(len(tablero)))
+	raiz = int(sqrt(len(tablero)))
 	length = len(palabra)
 	filas = []
-	listaNum = [n for n in range(0, rt**2)]
+	listaNum = [n for n in range(0, raiz**2)]
 
-	for i in range(0, rt):
-		filas += [listaNum[i * rt: (i * rt) + rt]]
-	for j in range(0, rt):
-		for k in range(0, rt):
-			if casilla == filas[j][k] and k <= (rt-length):
+	for i in range(0, raiz):
+		filas += [listaNum[i * raiz: (i * raiz) + raiz]]
+	for j in range(0, raiz):
+		for k in range(0, raiz):
+			if casilla == filas[j][k] and k <= (raiz-length):
 				return True
 
+"""
+Inserta la palabra en el tablero en la direccion y posicion dados
+"""
 def colocarPalabra(tablero, palabra, direccion, pos):
 
-	rt = int(sqrt(len(tablero)))
+	raiz = int(sqrt(len(tablero)))
 	length = len(palabra)
 	contador = 0
 
 	if direccion == "diag":
 		for i in range(0, length):
-			tablero[pos + (rt+1)*i] = palabra[contador]
+			tablero[pos + (raiz+1)*i] = palabra[contador]
 			contador += 1
 	elif direccion[:4] == "vert":
 		for i in range(0, length):
-			tablero[pos + rt*i] = palabra[contador]
+			tablero[pos + raiz*i] = palabra[contador]
 			contador += 1
 	elif direccion[:4] == "hori":
 		for i in range(0, length):
@@ -166,12 +195,43 @@ def colocarPalabra(tablero, palabra, direccion, pos):
 
 # Código opción 2:
 
-def buscarPalabras(list, tablero):
-	print(list)
+"""
+Busca una lista de palabras en la sopa, devuelve una lista con cada palabra encontrada, su posicion y direccion
+"""
+def buscarPalabras(palabras, tablero):
+
+	direcciones = ["hori1", "hori2", "vert1", "vert2", "diag"]
+	encuentros = []
+
+	for palabra in palabras:
+		length = len(palabra)
+		raiz = int(sqrt(len(tablero)))
+		for i in range(0, len(tablero)):
+			if ''.join(tablero[i:i+length]) in [palabra, palabra[::-1]]:
+				encuentros += [{'palabra': palabra, 'dir': 'horizontal' , 'pos': i}]
+			elif ''.join(tablero[i::raiz][:length]) in [palabra, palabra[::-1]]:
+				encuentros += [{'palabra': palabra, 'dir': 'vertical' , 'pos': i}]
+			elif ''.join(tablero[i::1+raiz][:length]) in [palabra, palabra[::-1]]:
+				encuentros += [{'palabra': palabra, 'dir': 'diag' , 'pos': i}]
+	[print(encuentro) for encuentro in encuentros]
+	return encuentros
+		
+	print(palabras)
 	imprimirTablero(tablero)
 
-#crearSopa(["TESTING", "HOLA", "PRUEBA", "WIRZT", "CASA", "BARCO"])
-#crearSopa(["TESTING", "HOLA", "PRUEBA", "WIRZT", "CASA", "BARCO", "DIAGONAL", "TELE", "PUÑO", "GANCHO"])
-tableroPrueba = ['Y', 'L', 'G', 'A', 'W', 'O', 'J', 'G', 'A', 'N', 'S', 'I', 'P', 'Z', 'O', 'B', 'I', 'A', 'R', 'C', 'D', 'C', 'E', 'T', 'C', 'Z', 'X', 'E', 'R', 'U', 'S', 'M', 'T', 'Z', 'S', 'A', 'R', 'E', 'A', 'L', 'O', 'H', 'B', 'P', 'T', 'J', 'D', 'G', 'M']
-main(["TESTING", "HOLA", "PRUEBA", "WIRZT", "CASA", "BARCO"], tableroPrueba)
-#imprimirTablero(tableroPrueba)
+"""
+Inicializa el programa con unos valores predefinidos para pruebas
+"""
+def start():
+	palabrasPrueba = []
+	
+	#tableroPrueba = ['Y', 'L', 'G', 'A', 'W', 'O', 'J', 'G', 'A', 'N', 'S', 'I', 'P', 'Z', 'O', 'B', 'I', 'A', 'R', 'C', 'D', 'C', 'E', 'T', 'C', 'Z', 'X', 'E', 'R', 'U', 'S', 'M', 'T', 'Z', 'S', 'A', 'R', 'E', 'A', 'L', 'O', 'H', 'B', 'P', 'T', 'J', 'D', 'G', 'M']
+	
+	with open("palabras", "r") as palabras:
+		for palabra in palabras.readlines():
+			palabrasPrueba += [palabra.strip()]
+		palabras.close()
+	tableroPrueba = crearSopa(palabrasPrueba)
+	main(palabrasPrueba, tableroPrueba)
+
+start()
