58a7b9de1297aca526700300e2c0c119a435d22c
Jfarizano <juanignaciofarizano@gmail.com>
2018-10-18 09:17:32 -0300

    Comentario

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ Por ejemplo, el tablero: A B C
 						 G H I
 es de tamaño 3x3, por lo tanto tiene 9 elementos y está representado por la lista:
 ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+Las palabras se encuentran en el archivo "palabras.txt", donde cada línea representa una palabra.
+El tablero de la sopa de letras se encuentra en el archivo "tablero.txt".
+En caso que no exista algún archivo se creará uno vacío. 
+Para la opción 1 el archivo "palabras.txt" no debe estar vacío, en caso de que no exista el archivo
+"tablero.txt" se creará uno nuevo con la sopa resultante, y en caso de que exista, se sobreescribirá.
+Para la opción 2 ambos archivos deberán existir y ser distintos de vacío, se supone que el tablero
+que se encuentra en "tablero.txt" es válido y contiene las palabras a buscar.
 """
 
 # Funciones principales
